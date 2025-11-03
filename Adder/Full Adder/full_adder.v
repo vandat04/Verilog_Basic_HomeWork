0ea174f8f80d9e13ff444588fee35d40204ab0f0
vandat04 <truongvandat02112004@gmail.com>
2025-11-04 06:56:33 +0700

    Update full_adder.v

--- Adder/Full Adder/full_adder.v
+++ Adder/Full Adder/full_adder.v
@@ -2,14 +2,15 @@ module full_adder(
   input wire a,
   input wire b, 
   input wire c,
-  output wire sum, carry
+  output wire sum
+  output wire carry
 );
 
 wire s;
 wire[1:0] ca;
 
 half_adder ha_1(.(a_in(a) , b_in(b) , sum_out(s) , carry_out(ca[0]));
-half_adder ha_1(.(a_in(s) , b_in(c) , sum_out(sum) , carry_out(ca[1]));
+half_adder ha_2(.(a_in(s) , b_in(c) , sum_out(sum) , carry_out(ca[1]));
 
 assign carry = ca[0] | ca[1];
 
