0d3be71abf27b20394bcf4b0ae73548439a8245d
vandat04 <truongvandat02112004@gmail.com>
2026-01-24 10:16:07 +0700

    Update alu.v

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,6 +1,6 @@
 //Carry: Phép cộng khi bit vượt khỏi MSB, phép trừ nếu như không mượn
 //Zero: Khi phép toán bằng = 0
-//Negative: Khi kết quả là âm
+//Negative: Khi kết quả là âm a < b
 
 module alu (
 	input wire [7:0] a,
@@ -15,40 +15,47 @@ module alu (
 reg [8:0] out; 
 
 always @(*) begin
-	    y        = 8'b0;
         out      = 9'b0;
-        zero     = 1'b0;
-        negative = 1'b0;
+        
         carry    = 1'b0;
 
 	case (ctrl)
 		3'b000: 
 			begin
-				y[7:0] = a[7:0] & b[7:0];	
+				y[7:0]   = a[7:0] & b[7:0];
+                zero     = (y == 0) ? (1'b1) : (1'b0);  
+                carry    = 1'b0;
+                negative = 1'b0;
 			end
 		3'b001:
 			begin
-				y[7:0] = a[7:0] | b[7:0];
+				y[7:0]   = a[7:0] | b[7:0];
+                zero     = (y == 0) ? (1'b1) : (1'b0);
+                carry    = 1'b0;
+                negative = 1'b0;  
 			end
 		3'b010:
 			begin
 				out[8:0] = a[7:0] + b[7:0];
-                y[7:0] = out[7:0];
+                y[7:0]   = out[7:0];
+                zero     = (y == 0) ? (1'b1) : (1'b0);  
                 carry    = out[8];
+                negative = 1'b0;
 			end
 		3'b110:
 			begin
 				out[8:0] = a[7:0] - b[7:0];
-                y[7:0] = out[7:0];
+                y[7:0]   = out[7:0];
+                zero     = (y == 0) ? (1'b1) : (1'b0);  
                 carry    = out[8];
+                negative = y[7];
 			end
 		default: 
 			begin
                 // NOT a math operation → flags = 0
             end	
 	endcase
-        zero     = (y == 0) ? (1'b1) : (1'b0);
-        negative = y[7];
+          
 end
 
 endmodule
