e729aa3c5d70211f8c99a88c58ce6084076fb076
vandat04 <truongvandat02112004@gmail.com>
2026-01-24 10:11:50 +0700

    Update alu.v

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,1 +1,54 @@
+//Carry: Phép cộng khi bit vượt khỏi MSB, phép trừ nếu như không mượn
+//Zero: Khi phép toán bằng = 0
+//Negative: Khi kết quả là âm
 
+module alu (
+	input wire [7:0] a,
+	input wire [7:0] b,
+	input wire [2:0] ctrl,
+	output reg [7:0] y,
+	output reg zero,
+	output reg negative,
+	output reg carry
+);
+
+reg [8:0] out; 
+
+always @(*) begin
+	    y        = 8'b0;
+        out      = 9'b0;
+        zero     = 1'b0;
+        negative = 1'b0;
+        carry    = 1'b0;
+
+	case (ctrl)
+		3'b000: 
+			begin
+				y[7:0] = a[7:0] & b[7:0];	
+			end
+		3'b001:
+			begin
+				y[7:0] = a[7:0] | b[7:0];
+			end
+		3'b010:
+			begin
+				out[8:0] = a[7:0] + b[7:0];
+                y[7:0] = out[7:0];
+                carry    = out[8];
+			end
+		3'b110:
+			begin
+				out[8:0] = a[7:0] - b[7:0];
+                y[7:0] = out[7:0];
+                carry    = out[8];
+			end
+		default: 
+			begin
+                // NOT a math operation → flags = 0
+            end	
+	endcase
+        zero     = (y == 0) ? (1'b1) : (1'b0);
+        negative = y[7];
+end
+
+endmodule
